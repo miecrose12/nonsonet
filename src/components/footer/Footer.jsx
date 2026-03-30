90f49819849e03ce56d40ccc28af74150268a831
miecrose12 <itobiyeblossom75@gmail.com>
2026-03-30 16:10:19 +0100

    finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Footer.css';
+import './footer.css';
 
 function Footer() {
   const [email, setEmail] = useState('');
